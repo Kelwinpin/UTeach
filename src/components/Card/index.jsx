dea1c05c37437a7c55f1145962365a91d2736902
Kelwinpin <kelwincomw@gmail.com>
2025-05-05 22:31:38 -0300

    add reponsive to card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,31 +8,31 @@ export default function Card({
   href,
 }) {
   return (
-    <article
-      className="rounded-xl border border-gray-200 bg-white p-6 shadow-lg transition hover:shadow-xl"
-      itemScope
-      itemType="https://schema.org/Course"
-    >
-      <span
-        className={`inline-block px-2 py-1 text-xs font-semibold rounded-md mb-2 ${labelColor}`}
-      >
-        {label}
-      </span>
+    <div className="relative rounded-xl border border-gray-200 bg-white p-6 shadow-lg transition hover:shadow-xl h-60 w-56 flex flex-col items-center gap-2">
+        <article
+          itemScope
+          itemType="https://schema.org/Course"
+        >
+          <span
+              className={`inline-block px-2 py-1 text-xs font-semibold rounded-md mb-2 ${labelColor}`}
+          >
+              {label}
+          </span>
 
-      <h2 className="text-lg font-semibold text-gray-900 mb-2" itemProp="name">
-        {title}
-      </h2>
+          <h2 className="text-md font-semibold text-gray-900 mb-2" itemProp="name">
+              {title}
+          </h2>
 
-      <p className="text-sm text-gray-600 mb-4" itemProp="description">
-        {description}
-      </p>
-
-      <Link
-        href={href}
-        className="inline-block border border-blue-500 text-blue-600 px-4 py-2 text-sm font-semibold rounded hover:bg-blue-50 transition"
-      >
-        Take Lesson
-      </Link>
-    </article>
+          <p className="text-xs text-gray-600 mb-4" itemProp="description">
+              {description}
+          </p>
+        </article>
+        <Link
+            href={href}
+            className="inline-block border-2 border-blue-500 text-blue-600 px-9 py-2 text-sm font-semibold rounded-lg hover:bg-blue-50 transition absolute bottom-4"
+        >
+         Take Lesson
+        </Link>
+    </div>
   );
 }
