138af40b35ab6b343b63d382badba9c31391e255
Kelwinpin <kelwincomw@gmail.com>
2025-05-07 22:37:24 -0300

    add mobile

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -12,10 +12,10 @@ export default function Features() {
   }
 
   return (
-    <section className="py-24 px-6 flex items-center justify-center gap-15">
-        <div>                  
+    <section className="flex md:flex-row flex-col items-center justify-center gap-15">
+        <div className="flex flex-col px-14">                  
             <h2 className="text-3xl md:text-5xl font-semibold text-[#0B0B28] mb-8">All the cool features</h2>
-            <p className="text-lg text-[#0B0B28]/80 mb-8 w-lg">
+            <p className="md:text-lg text-[#0B0B28]/80 mb-8 w-lg">
                 Mauris consequat, cursus pharetra et, habitasse rhoncus quis odio ac. In et dolor eu donec maecenas nulla. 
                 Cum sed orci, sit pellentesque quisque feugiat cras ullamcorper. Ultrices in amet, ullamcorper non viverra a, neque orci.
             </p>
@@ -26,7 +26,7 @@ export default function Features() {
                 View all features →
             </Link>
         </div>
-        <div className="mt-30">
+        <div>
             <div className="relative">
                 <div className="absolute md:-top-50 left-60 inset-0 z-1 bg-[url('/images/bg-faq.webp')] bg-no-repeat  pointer-events-none" />    
                 <div className="absolute -left-40 -top-25 h-80 md:h-auto md:-top-40 md:left-10 inset-0 z-0 bg-[url('/images/blob.webp')] bg-no-repeat pointer-events-none" />
